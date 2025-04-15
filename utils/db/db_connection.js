cc8ffd70f73af87187123f6bc6e87fb83aafa5a6
bungongtoto <bungongkingsley@gmail.com>
2025-04-15 12:20:26 +0100

    add db connection

---
+++ utils/db/db_connection.js
@@ -0,0 +1,11 @@
+const Pool = require('pg').Pool;
+
+const Pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB,
+    password: process.env.DB_PASS,
+    port: process.env.DB_PORT
+});
+
+module.exports = Pool;
